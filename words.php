9c6d785cb5d4171941fe96a7d687640844096db2
Gabri <gperezpereira@danielcastelao.org>
2023-12-12 13:57:37 +0100

    plugin terminado

--- words.php
+++ words.php
@@ -47,10 +47,70 @@ $nonSwearingWords= array("Miercoles",
                     "Pancho"
  );
 
-//Cambiar WordPress por WordPressDAM
+//Cambiar palabras malsonantes por otras más suaves en el contenido usando la base de datos
 function renym_wordpress_typo_fix( $text ) {
-    global $swearingWords, $nonSwearingWords;
+    $words = selectData();
+    foreach ($words as $result){
+        $swearingWords[] = $result->palabrotas;
+        $nonSwearingWords[] = $result->eufemismo;
+    }
     return str_replace($swearingWords, $nonSwearingWords, $text);
 }
 
 add_filter( 'the_content', 'renym_wordpress_typo_fix' );
+
+/*
+ * Añadir una tabla a la Base de Datos
+ */
+
+function createTable(){
+    global $wpdb;
+    //Nombre de la tabla
+    $table_name = $wpdb->prefix . "words";
+    //Charset de la tabla
+    $charset_collate = $wpdb->get_charset_collate();
+    //Sentencia SQL
+    $sql = "CREATE TABLE IF NOT EXISTS $table_name (
+        id mediumint(9) NOT NULL AUTO_INCREMENT,
+        palabrotas varchar(255) NOT NULL,
+        eufemismo varchar(255) NOT NULL,
+        PRIMARY KEY  (id)
+    ) $charset_collate;";
+    //Incluir el fichero para poder ejecutar dbDelta
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
+}
+
+add_action("plugins_loaded", "createTable");
+
+/*
+ * Insertar datos en la tabla (si ya están añadidos no se añaden)
+ */
+function insertData(){
+    global $wpdb, $swearingWords, $nonSwearingWords;
+    $table_name = $wpdb->prefix . "words";
+    $flag = $wpdb->get_results("SELECT * FROM $table_name");
+    if (count($flag)==0){
+        for ($i = 0; $i < count($swearingWords); $i++){
+            $wpdb->insert(
+                $table_name,
+                array(
+                    'palabrotas' => $swearingWords[$i],
+                    'eufemismo' => $nonSwearingWords[$i]
+                )
+            );
+        }
+    }
+}
+
+add_action("plugins_loaded", "insertData");
+
+/*
+ * Seleccionar datos de la tabla
+ */
+function selectData(){
+    global $wpdb;
+    $table_name = $wpdb->prefix . "words";
+    $results = $wpdb->get_results("SELECT * FROM $table_name");
+    return $results;
+}
